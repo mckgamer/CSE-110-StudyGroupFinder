e9d1d5fa46a4a21023dd40dac4aed8e098198007
mckgamer <mckgamer@yahoo.com>
2011-10-21 17:41:17 -0700

    Near Completed? User to better test Database.

--- StudyGroupFinder/src/domainlogic/User.java
+++ StudyGroupFinder/src/domainlogic/User.java
@@ -2,10 +2,16 @@ package domainlogic;
 
 import database.UserData;
 
+/** This is the main 'session' object for the program. There is one User object that represents
+ *  the person using the program.
+ *  
+ * @author Michael Kirby
+ *
+ */
 public class User {
 	
 	/** Enumeration for status off the user */
-	enum Logged { USER, ADMIN, INVALID, LOGGEDOFF }
+	public enum Logged { USER, ADMIN, INVALID, LOGGEDOFF }
 	
 	/** Specifies if the user is logged in */
 	Logged status = Logged.LOGGEDOFF;
@@ -13,18 +19,43 @@ public class User {
 	/** The {@link UserData} of the current user. */
 	UserData user = null;
 	
-	/** Returns the Logged status of this User 
+	/** Constructs a User object for the current session
 	 * 
-	 * @return the Logged status of this user
+	 * @param s the {@link Logged} status of this User.
+	 * @param ud the {@link UserData} associated with this User.
 	 */
-	Logged getStatus() {
+	public User(Logged s, UserData ud) {
+		status = s;
+		user = ud;
+	}
+	
+	/** Returns the {@link Logged} status of this User 
+	 * 
+	 * @return the {@link Logged} status of this user
+	 */
+	public Logged getStatus() {
 		return status;
 	}
 	
+	/** Sets the {@link UserData} of this User to the given UserData
+	 * 
+	 * @param ud the given {@link UserData}.
+	 */
+	public void setUserData(UserData ud) {
+		user = ud;
+	}
+
+	/** Sets the {@link Logged} status of this User.
+	 * 
+	 * @param s the {@link Logged} status to set to.
+	 */
+	public void setStatus(Logged s) {
+		status = s;
+	}
+	
 	/** Updates the users Logged status to LOGGEDOFF and nullifies the UserData */
-	void logoff() {
+	public void logoff() {
 		status = Logged.LOGGEDOFF;
 		user = null;
 	}
-
 }
