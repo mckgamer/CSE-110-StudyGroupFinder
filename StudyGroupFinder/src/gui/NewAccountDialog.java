f1e8dcb0eb3cf131c5c21212bc0315594a535199
mckgamer <mckgamer@yahoo.com>
2011-10-22 20:07:42 -0700

    Cleaned up GUI code with comments and seperated UserMenu into its own class.

--- StudyGroupFinder/src/gui/NewAccountDialog.java
+++ StudyGroupFinder/src/gui/NewAccountDialog.java
@@ -9,6 +9,15 @@ import javax.swing.JDialog;
 
 public class NewAccountDialog extends JDialog implements ActionListener, PropertyChangeListener {
 
+	GUIFrame parent;
+	
+	public NewAccountDialog(GUIFrame parent) {
+		super(parent, true);
+		this.parent = parent;
+		setTitle("Create New Account");
+		setSize(400,300);
+	}
+	
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
 		// TODO Auto-generated method stub
--- StudyGroupFinder/src/gui/UpdateUserDialog.java
+++ StudyGroupFinder/src/gui/UpdateUserDialog.java
@@ -7,18 +7,38 @@ import java.beans.PropertyChangeListener;
 
 import javax.swing.JDialog;
 
+/** A JDialog for updating the users profile. This is displayed when the user selects
+ * My Profile.
+ */
 public class UpdateUserDialog extends JDialog implements ActionListener, PropertyChangeListener {
 
+	/** The {@link GUIFrame} that is displaying this NewGroupDialog */
+	GUIFrame parent;
+	
+	/** Constructs this UpdateUserDialog object using the {@link GUIFrame} that will display it.
+	 * 
+	 * @param parent the {@link GUIFrame} that will display this UpdateUserDialog.
+	 */
+	public UpdateUserDialog(GUIFrame parent) {
+		super(parent, true);
+		
+		//Give this Dialog a reference to its GUIFrame parent
+		this.parent = parent;
+		
+		//Set the size, position, and title of this dialog
+		setTitle("Update My Profile");
+		setSize(400,300);
+		setLocationRelativeTo(null);
+	}
+	
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
 		// TODO Auto-generated method stub
-		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 }
