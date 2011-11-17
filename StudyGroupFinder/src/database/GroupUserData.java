b6e3a036ce346da7afcaaed0ff3d296ab7d4cad7
mckgamer <mckgamer@yahoo.com>
2011-11-17 00:49:20 -0800

    Added two new Data implementations. To be implemented fully for phase two.

---
+++ StudyGroupFinder/src/database/GroupUserData.java
@@ -0,0 +1,23 @@
+package database;
+
+/**
+ * This {@link Data} implementation is used in any case where a relationship between a User
+ * and a group is required.
+ * 
+ * @author Michael Kirby
+ */
+public class GroupUserData implements Data {
+
+	@Override
+	public boolean validate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ StudyGroupFinder/src/database/SearchData.java
@@ -0,0 +1,23 @@
+package database;
+
+/** 
+ * This {@link Data} implementation is used for handling search results in Study Group
+ * Finder.
+ * 
+ * @author Michael Kirby
+ */
+public class SearchData implements Data {
+
+	@Override
+	public boolean validate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
